f9512603406e373eb81db2466d65e098d02c87ae
JUlia <doujulia@gmail.com>
2021-02-11 23:38:38 -0800

    deleted junk files

--- Documents/CIS-18A/Project/OrderInput.java
+++
@@ -1,9 +0,0 @@
-class OrderInput {
-public static void main(String args[])
-throws java.io.IOException {
-	String order;
-	System.out.print("Press any key followed by ENTER: ");
-	order = (String) System.in.read();	// read character from keyboard
-	System.out.println("Your key is: " + order);
-	}
-}
